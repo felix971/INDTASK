f28b9ea57d98bb7c6d384e3ac5ef3f8b0dcf010f
felix971 <felixzqy@gmail.com>
2025-06-01 02:37:22 +1000

    123

--- api/test.php
+++ api/test.php
@@ -1,8 +1,3 @@
 <?php
-header('Content-Type: application/json');
-echo json_encode([
-    'message' => 'PHP在Vercel上工作正常！',
-    'timestamp' => date('Y-m-d H:i:s'),
-    'status' => 'success'
-]);
+echo "PHP works! Time: " . date('Y-m-d H:i:s');
 ?>
